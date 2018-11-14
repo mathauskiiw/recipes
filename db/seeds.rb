ab44a9eab4c88dc3f446a804740c2dd326288a5b
pedro <pedro.uece.2016@gmail.com>
2018-11-14 18:41:32 -0200

    categorias e levels criados pelo seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', name: 'AdminDefault') if Rails.env.development?
+
+Category.create!(name:"Entrada")
+Category.create!(name:"Prato Principal")
+Category.create!(name:"Sobremesa")
+
+Level.create!(name:"Amador")
+Level.create!(name:"Estudante")
+Level.create!(name:"Profissional")
